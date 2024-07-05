a225108682bd6e3ee1cdd38bbb4ea7fe84bd3681
MMiah <107433786+94769476@users.noreply.github.com>
2024-07-05 11:51:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const screen = document.querySelector(".screen")
+const buttons = document.querySelectorAll("button")
+
+var button
+
+var equal
+
+
+/*buttons.forEach(button) => {
+    button.addEventListener("click", (e) => );
+});*/
